5e07e28dde600ffa05428a7eb5644cacce41f6d2
IlyaK <ilya.k@pettersonapps.com>
2023-04-14 21:18:34 +0300

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Inthegametv",
+    platforms: [
+        .iOS(.v14), .tvOS(.v14)
+    ],
+    products: [
+        .library(name: "Inthegametv", targets: ["Inthegametv", "StorketService"]),
+        .library(name: "InthegametviOS", targets: ["InthegametviOS", "StorketService"]),
+    ],
+    targets: [
+        .binaryTarget(name: "Inthegametv", path: "Sources/Inthegametv/Inthegametv.xcframework"),
+        .binaryTarget(name: "InthegametviOS", path: "Sources/InthegametviOS/InthegametviOS.xcframework"),
+        .binaryTarget(name: "StorketService", path: "Sources/StorketService/StorketService.xcframework"),
+    ]
+)
---
+++ README.md
@@ -0,0 +1,17 @@
+# Inthegametv
+
+## Swift Package Manager
+### Introduction
+Inthegametv SDK version 2.1.0+ supports installation via [Swift Package Manager](https://swift.org/package-manager/).
+
+### Installation
+In Xcode go to: ```File -> Add Packages...```
+
+Enter the Inthegametv SDK GitHub repository - ```https://github.com/Inthegamesdk/InthegameFramework.git```
+
+Select the SDK version
+
+Select the tvOS or iOS version
+
+In your swift files: import Inthegametv or InthegametviOS module
+
