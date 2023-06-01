3ab8b3284b704572db8e2e326455f84b1dcf448a
IlyaK <ilya.k@pettersonapps.com>
2023-06-01 15:49:28 +0300

    SDK update

---
+++ Sources/StorketService/StorketService.xcframework/StorketService.xcframework/ios-arm64_x86_64-simulator/StorketService.framework/Headers/Storket.h
@@ -0,0 +1,18 @@
+//
+//  Storket.h
+//  Storket
+//
+//  Created by Daedalus on 21/10/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Storket.
+FOUNDATION_EXPORT double StorketVersionNumber;
+
+//! Project version string for Storket.
+FOUNDATION_EXPORT const unsigned char StorketVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Storket/PublicHeader.h>
+
+
