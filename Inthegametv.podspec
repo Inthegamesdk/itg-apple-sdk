8dea86df39eeb3f106a2d9e1a94805fba1d496ee
IlyaK1 <ilya.k@pettersonapps.com>
2025-07-18 12:18:38 +0300

    SDK update

--- Inthegametv.podspec
+++ Inthegametv.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
     s.name         = "Inthegametv"
-    s.version      = "2.6.22"
+    s.version      = "2.6.23"
     s.summary      = "Inthegametv SDK for iOS and tvOS"
     s.description  = "Inthegametv SDK for iOS and tvOS"
     s.homepage     = "www.inthegame.io"
--- Sources/SupportingFiles/PlayerControllerAndAdapters/ITGMediastreamPlatformAdapter.swift
+++ Sources/SupportingFiles/PlayerControllerAndAdapters/ITGMediastreamPlatformAdapter.swift
@@ -15,7 +15,7 @@ import MediastreamPlatformSDKiOS
 import ITGPlayerViewController
 #endif
 
-class ITGMediastreamPlatformAdapter: ITGPlayerAdapter {
+open class ITGMediastreamPlatformAdapter: ITGPlayerAdapter {
     
     weak public var delegate: ITGPlayerAdapterDelegate?
     var mdstrm: MediastreamPlatformSDK
@@ -29,7 +29,7 @@ class ITGMediastreamPlatformAdapter: ITGPlayerAdapter {
         mdstrm.events.removeListeners(eventNameToRemoveOrNil: nil)
     }
     
-    func setup() {
+    open func setup() {
         mdstrm.events.listenTo(eventName: "play") {
             self.delegate?.videoPlaying(self.getCurrentTime())
         }
@@ -48,15 +48,15 @@ class ITGMediastreamPlatformAdapter: ITGPlayerAdapter {
         })
     }
     
-    func startVideo(_ url: URL) {
+    open func startVideo(_ url: URL) {
  
     }
     
-    func getPlayerView() -> UIView? {
+    open func getPlayerView() -> UIView? {
         mdstrm.view
     }
     
-    func getVideoResolution() -> CGSize {
+    open func getVideoResolution() -> CGSize {
         let resolution = mdstrm.getResolution()
         let components = resolution.split(separator: "x")
         if components.count == 2, let width = Double(components.first!), let height = Double(components.last!) {
@@ -66,31 +66,31 @@ class ITGMediastreamPlatformAdapter: ITGPlayerAdapter {
         }
     }
     
-    func isPlaying() -> Bool {
+    open func isPlaying() -> Bool {
         mdstrm.checkIsPlaying()
     }
     
-    func play() {
+    open func play() {
         mdstrm.play()
     }
     
-    func pause() {
+    open func pause() {
         mdstrm.pause()
     }
     
-    func seek(_ time: TimeInterval) {
+    open func seek(_ time: TimeInterval) {
         mdstrm.seekTo(time)
     }
     
-    func getCurrentTime() -> TimeInterval {
+    open func getCurrentTime() -> TimeInterval {
         return Double(mdstrm.getCurrentTime()/1000)
     }
     
-    func getVideoLength() -> TimeInterval {
+    open func getVideoLength() -> TimeInterval {
         return Double(mdstrm.getDuration())
     }
     
-    func setVideoGravity(_ videoGravity: AVLayerVideoGravity) {
+    open func setVideoGravity(_ videoGravity: AVLayerVideoGravity) {
         if let playerViewController = mdstrm.playerViewController {
             playerViewController.videoGravity = videoGravity
         } else {
@@ -98,11 +98,11 @@ class ITGMediastreamPlatformAdapter: ITGPlayerAdapter {
         }
     }
     
-    func setSoundLevel(_ soundLevel: Float) {
+    open func setSoundLevel(_ soundLevel: Float) {
         mdstrm.volume = Int(soundLevel*100)
     }
     
-    func getSoundLevel() -> Float {
+    open func getSoundLevel() -> Float {
         return Float(mdstrm.volume/100)
     }
     
